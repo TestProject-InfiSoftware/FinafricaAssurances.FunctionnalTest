ff44dfb852e22c743498754667e604feeebc7c8f
Lou-LauriatHerosside <sekou.diabate@infisoftware.net>
2023-12-18 11:14:08 +0000

    project settings

---
+++ Finafrica.FunctionnalTest.E2E.UI/cypress/e2e/DeclarationSinistre.cy.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
+describe("Declaration d'un sinistre", () => {
+  it('Creation compte utilisateur', () => {
+    cy.visit('https://gestion-sinistre.suntelecoms.com/#/home')
+  })
+})
